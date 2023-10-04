3e6a3288a1434705f01e539a1998e2242ff37d91
unknown <bilalpt>
2023-10-04 22:19:28 +0530

    given user role on models.py

--- petboarding/models.py
+++ petboarding/models.py
@@ -7,12 +7,20 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
+    USER_ROLES=(
+        ('boarduser','Boarduser'),
+        ('taker','Taker'),
+        ('admin','Admin'),
+    )
+
+
     username=models.CharField(max_length=150,unique=True)
     email=models.EmailField(max_length=200,unique=True)
     phone=models.CharField(max_length=14)
     # profile_image = models.ImageField(upload_to='profile_image', null=True, blank=True)
 
     password=models.CharField(max_length=200)
+    roles=models.CharField(max_length=20,choices=USER_ROLES,default='boarduser')
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
--- petcare/models.py
+++ petcare/models.py
@@ -1,3 +1,18 @@
-from django.db import models
+# from django.db import models
+# from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+# # Create your models here.
+
+
+
+# class PetcareUser(AbstractUser):
+#     Takerusername=models.CharField(max_length=150,unique=True)
+#     Takeremail=models.EmailField(max_length=200,unique=True)
+#     Takerphone=models.CharField(max_length=14)
+#     # profile_image = models.ImageField(upload_to='profile_image', null=True, blank=True)
+
+#     password=models.CharField(max_length=200)
+
+#     USERNAME_FIELD = 'Takeremail'
+#     REQUIRED_FIELDS = ['username']
